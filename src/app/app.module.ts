e8d595e0b8f708b4934379e0ecf8316edc22bd2f
icodeatgit <codeatgit@gmail.com>
2021-12-18 10:17:20 +0530

    Initial UI Design ayout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodosComponent } from './todos/todos.component';
+import { TaskviewComponent } from './pages/taskview/taskview.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosComponent,
+    TaskviewComponent
   ],
   imports: [
     BrowserModule,
