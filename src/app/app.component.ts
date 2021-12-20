e702a4c7a3c2efefbd288edf880378c0042e6717
icodeatgit <codeatgit@gmail.com>
2021-12-20 19:18:56 +0530

    commented angular animation routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
+import { animate, query, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  // animations: [
+  //   trigger('routeAnimation', [
+  //     transition('* => *',[
+  //       query(':enter', [
+  //         style({
+  //           opacity: 0,
+  //           display: 'block'
+  //         }),
+  //         animate(1000), style({
+  //           opacity: 1
+  //         })
+  //       ], {
+  //         optional: true
+  //       })
+  //     ])     
+  //   ])
+  // ]
 })
 export class AppComponent {
-  title = 'notes';
+  
+  // prepareRoute(outlet: RouterOutlet) {
+  //   if (outlet.isActivated) {
+  //     return outlet.activatedRoute.snapshot.url;
+  //   }
+  //   return outlet?.activatedRouteData?.['animation'];
+  // }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -18,7 +19,8 @@ import { NewTaskComponent } from './pages/new-task/new-task.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
